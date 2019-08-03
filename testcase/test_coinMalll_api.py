06a7f4b620282b27e8184e14137688f215c849b1
caryangBingo <zhiyuan1005873@gmail.com>
2019-08-03 18:45:23 +0800

    用例算法

--- testcase/test_coinMalll_api.py
+++ testcase/test_coinMalll_api.py
@@ -15,6 +15,7 @@ import allure
 import pytest
 import yaml
 import openpyxl
+import itertools
 sys.path.append("../common/")
 from assertpy.assertpy import assert_that, fail
 # from ddt import ddt, file_data
@@ -41,77 +42,97 @@ excel_filename = '../data/broker-testcase_src.xlsx'
 # http://devmsmobapi.lanmang.me/api/EnterpriseQueryMobile/GetEnterpriseById?entId=1042&uid=20165943
 
 
-def readExcelData():
-    case2 = OrderedDict([('A', ['A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9']),
-                         ('B', ['B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9']),
-                         ('C', ['C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9']),
-                         ('D', ['D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9']),
-                         ('E', ['E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'E9'])])
-    exp_case2 = OrderedDict([('F', ['F3', 'F4']), ('G', ['G3', 'G4'])])
+class OrthogonalCaseProcessing(object):
+    """docstring for OrthogonalCaseProcessing"""
 
-    sheet_cell = json.loads(json.dumps(oat.genSets(case2)))
-    sheet_exp = json.loads(json.dumps(oat.genSets(exp_case2)))
-    wb = openpyxl.load_workbook(filename=excel_filename)
-    sheet = wb.active
-    # print(sheet['A3'].value)
-    # print(sheet['B3'].value)
-    # rows = sheet.max_row  # 获取行数
-    # cols = sheet.max_column  # 获取列数
-    # print(rows, cols)
+    def __init__(self):
+        self.workbook = openpyxl.load_workbook(filename=excel_filename)
 
-    for cellDict in sheet_cell:
-        #params_case2 = ''
-        #A_cellDict = cellDict.get('A')
-        #B_cellDict = cellDict.get('B')
-        #C_cellDict = cellDict.get('C')
-        #D_cellDict = cellDict.get('D')
-        #E_cellDict = cellDict.get('E')
-        yield cellDict.get('A')
-        # print(A_cellDict, B_cellDict, C_cellDict, D_cellDict, E_cellDict)
-        # print(sheet[A_cellDict].value, sheet[B_cellDict].value,
-        #      sheet[C_cellDict].value, sheet[D_cellDict].value,
-        #      sheet[E_cellDict].value)
-        '''
-        params_case2 += '{'
-        params_case2 += '"aboutId": {0}, "uid": {1}, "parentId": {2}, "pageIndex": {3}, "pageSize": {4}'.format(
-            sheet[A_cellDict].value, sheet[B_cellDict].value, sheet[C_cellDict].value, sheet[D_cellDict].value, sheet[E_cellDict].value)
-        params_case2 += '}'
-        '''
-        # print(params_case2)
-        # yield eval(params_case2)
+    def readExcelData(self):
+        # case2 = OrderedDict([('A', ['A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9']),
+        #                     ('B', ['B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9']),
+        #                     ('C', ['C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9']),
+        #                     ('D', ['D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9']),
+        #                     ('E', ['E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'E9'])])
+        # exp_case2 = OrderedDict([('F', ['F3', 'F4']), ('G', ['G3', 'G4'])])
+
+        # sheet_cell = json.loads(json.dumps(oat.genSets(case2)))
+        # sheet_exp = json.loads(json.dumps(oat.genSets(exp_case2)))
+
+        sheet = self.workbook.active
+        rows = sheet.max_row  # 获取行数
+        cols = sheet.max_column  # 获取列数
+
+        req_paramsRows = list(map(str, (range(3, rows + 1))))
+        req_paramsCols = list(chr(i + 65) for i in range(0, cols - 2))
+        req_paramsName = list(map(lambda x: x + str(2), req_paramsCols))
+
+        case_map = [(key, list(map(lambda x:''.join(x), zip(
+            [key] * len(req_paramsRows), req_paramsRows)))) for key in req_paramsCols]
+
+        case2 = OrderedDict(case_map)
+        sheet_cell = json.loads(json.dumps(oat.genSets(case2)))
+
+        """
+        for col in req_paramsCols:
+            col_list = []
+            for row in req_paramsRows:
+                col_list.append(col + row)
+            yield ("('{}': {})".format(col, col_list))
+            # print('{}: {}'.format(col, col + str(row)))
+        """
+        cellData = list(list(map(lambda x: cellData.get(x), req_paramsCols))
+                        for cellData in sheet_cell)
+
+        cellData = list(
+            list(map(lambda x: str(sheet[x].value), data)) for data in cellData)
+
+        req_paramsName = list(map(lambda x: sheet[x].value, req_paramsName))
+
+        # params_case2 += '{'
+        # params_case2 += '"aboutId": {0}, "uid": {1}, "parentId": {2}, "pageIndex": {3}, "pageSize": {4}'.format(
+        #    sheet[A_cellDict].value, sheet[B_cellDict].value, sheet[C_cellDict].value, sheet[D_cellDict].value, sheet[E_cellDict].value)
+        # params_case2 += '}'
+        req_paramsData = list(map(lambda x: dict(
+            zip(req_paramsName, x[0])), itertools.groupby(cellData)))
+
+        yield req_paramsData
+
+    # readExcelData()
+    #print([i for i in readExcelData()])
+
+    """
+    for cellDict in sheet_exp:
+        F_cellDict = cellDict.get('F')
+        G_cellDict = cellDict.get('G')
+        print(C_cellDict, D_cellDict)
+        print(sheet[C_cellDict].value, sheet[D_cellDict].value)
+    """
+
+    @allure.step
+    @pytest.mark.parametrize('params', [i for i in CaseProcessing.readExcelData(self)][0])
+    def test_api_getCommentList_request(self, params):
+        print(params)
+        r = requests.get(coinMall_url + apiPath,
+                         params=params,
+                         headers=headers)
+        reqUrl = r.url
+        result = r.json()
+        # elapsedTime = r.elapsed.total_seconds()
+        # assertEqual(r.status_code, http_statusCode)
+        # assertEqual(r.status_code, http_statusCode)
+        # assert result['code'] == 0
+        try:
+            assert_that(result.get('code')).is_equal_to(0)
+        except AssertionError:
+            assert_that(result.get('code')).is_equal_to(-1)
+        # assert Assertions.assert_msg(
+        #    result['message'], responsesBody['message'])
+        # assert Assertions.assert_body(
+        #    result['bodyMessage'], responsesBody['bodyMessage'])
+        # assert Assertions.assert_subCode(
+        #    result['subCode'], responsesBody['subCode']
 
 
-print([i for i in readExcelData()])
-
-"""
-for cellDict in sheet_exp:
-    F_cellDict = cellDict.get('F')
-    G_cellDict = cellDict.get('G')
-    print(C_cellDict, D_cellDict)
-    print(sheet[C_cellDict].value, sheet[D_cellDict].value)
-"""
-
-"""
-@allure.step
-@pytest.mark.parametrize('params', [i for i in readExcelData()])
-def test_api_getCommentList_request(params):
-    r = requests.get(coinMall_url + apiPath,
-                     params=params,
-                     headers=headers)
-    reqUrl = r.url
-    result = r.json()
-    # elapsedTime = r.elapsed.total_seconds()
-    # assertEqual(r.status_code, http_statusCode)
-    # assertEqual(r.status_code, http_statusCode)
-    # assert result['code'] == 0
-    try:
-        assert_that(result.get('code')).is_equal_to(0)
-    except AssertionError:
-        assert_that(result.get('code')).is_equal_to(-1)
-    # assert Assertions.assert_msg(
-    #    result['message'], responsesBody['message'])
-    # assert Assertions.assert_body(
-    #    result['bodyMessage'], responsesBody['bodyMessage'])
-    # assert Assertions.assert_subCode(
-    #    result['subCode'], responsesBody['subCode']
-"""
+if __name__ == '__main__':
+    CaseProcessing = OrthogonalCaseProcessing()
